b52bcc1cb733dfecc3fdd70adf775bdd36b2e3b6
mngerasimenko <mngerasimenko@gmail.com>
2021-03-07 18:12:51 +0400

    Added JFrame and JMenuBar on QuizCardBuilder

--- src/main/java/ru/betterstop/QuizCardBuilder.java
+++ src/main/java/ru/betterstop/QuizCardBuilder.java
@@ -1,6 +1,7 @@
 package ru.betterstop;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class QuizCardBuilder {
@@ -15,6 +16,33 @@ public class QuizCardBuilder {
     }
 
     public void build() {
+        frame = new JFrame("Создание карточек с вопросами");
+        JPanel mainPanel = new JPanel();
 
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Файл");
+        JMenuItem newMenuItem = new JMenuItem("Создать");
+        JMenuItem saveMenuItem = new JMenuItem("Сахранить");
+        JMenuItem exitMenuItem = new JMenuItem("Выход");
+
+        newMenuItem.addActionListener(listner -> {
+            System.out.println("New");
+        });
+        saveMenuItem.addActionListener(listner -> {
+            System.out.println("Save");
+        });
+        exitMenuItem.addActionListener(listner -> {
+            frame.dispose();
+        });
+
+        fileMenu.add(newMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.add(exitMenuItem);
+        menuBar.add(fileMenu);
+        frame.setJMenuBar(menuBar);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(500, 600);
+        frame.setVisible(true);
     }
 }
