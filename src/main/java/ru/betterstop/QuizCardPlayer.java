efc14e743f2c3b24945d08eb95d69ad192ebc066
mngerasimenko <mngerasimenko@gmail.com>
2021-03-08 15:18:32 +0400

    A form for uploading cards has been created

--- src/main/java/ru/betterstop/QuizCardPlayer.java
+++ src/main/java/ru/betterstop/QuizCardPlayer.java
@@ -1,8 +1,61 @@
 package ru.betterstop;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class QuizCardPlayer {
 
     private JTextArea question;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private QuizCard currentCard;
+    private int currentCardId;
+    private JFrame frame;
+    private JButton nextButton;
+    private boolean isShowAnswer;
+
+    public static void main(String[] args) {
+        QuizCardPlayer reader = new QuizCardPlayer();
+        reader.readCard();
+    }
+
+    public void readCard() {
+        frame = new JFrame("Карточки с вопросами");
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+
+        question = new JTextArea(10, 20);
+        question.setFont(bigFont);
+        question.setLineWrap(true);
+        question.setEditable(false);
+
+        JScrollPane qScroll = new JScrollPane(question);
+        qScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        qScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        nextButton = new JButton("Следующая карточка");
+        nextButton.addActionListener(listner -> {
+            System.out.println("next");
+        });
+        mainPanel.add(qScroll);
+        mainPanel.add(nextButton);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu= new JMenu("Файл");
+        JMenuItem loadMenuItem = new JMenuItem("Загрузить набор карточек");
+
+        loadMenuItem.addActionListener(listener -> {
+            System.out.println("load");
+        });
+
+        fileMenu.add(loadMenuItem);
+        menuBar.add(fileMenu);
+        frame.setJMenuBar(menuBar);
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(640, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
